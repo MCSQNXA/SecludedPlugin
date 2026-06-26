c83bf467373c2276c4ca961eae2a22b3797307ee
MCSQNXA <mcsqnxa@qq.com>
2026-06-26 23:45:42 +0800

    优化代码

--- src/mcsq/nxa/secluded/Demo.java
+++ src/mcsq/nxa/secluded/Demo.java
@@ -10,8 +10,7 @@ import mcsq.nxa.secluded.plugin.SecPluginHandler;
  * @CreateTime 2024-08-04 下午11:04
  * @Description 例子
  */
-public class Demo implements SecPluginHandler {
-
+public class Demo implements Runnable, SecPluginHandler {
     /**
      * @CreateTime 2024-08-04 23:04:58
      * @Description 主函数
@@ -24,22 +23,16 @@ public class Demo implements SecPluginHandler {
         SecPlugin.printW("黄色日志");
 
         for (int i = 0; i < 1; i++) {
-            //noinspection Convert2Lambda
-            SecPlugin.thread(new Runnable() {
-                @Override
-                public void run() {
-                    SecPlugin plugin = new SecPlugin("ws://127.0.0.1:24804/?token=y5s9WERi");// 令牌 方式一
-                    plugin.setAccessToken("y5s9WERi");// 令牌 方式二
-                    plugin.setHandler(new Demo());
-                    plugin.start();// 启动插件
-                    //plugin.stop();// 停止插件
-                }
-            });
+            SecPlugin.thread(new Demo());
         }
-
-
     }
 
+    @Override
+    public void run() {
+        SecPlugin plugin = new SecPlugin("ws://127.0.0.1:24804/?token=y5s9WERi");
+        plugin.setHandler(this);
+        plugin.start();
+    }
 
     /**
      * @CreateTime 2024-08-04 23:22:05
